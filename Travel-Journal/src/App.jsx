8651a96e1b96386ac26c26144f984caf3b45ca13
Katie Schwerer <89220941+Katie-Schwerer@users.noreply.github.com>
2026-09-14 23:55:42 +0000

    Travel Journay is done

--- Travel-Journal/src/App.jsx
+++ Travel-Journal/src/App.jsx
@@ -1,12 +1,27 @@
 import React from "react"
 import './App.css'
 import Header from "./components/Header"
+import Entry from "./components/Entry"
+import data from "./data.js";
 
 function App() {
 
   return (
     <>
       <Header />
+      <main className="container">
+        {data.map((country) => (
+          <Entry
+            key={country.id}
+            img={country.img}
+            title={country.title}
+            country={country.country}
+            googleMapsLink={country.googleMapsLink}
+            dates={country.dates}
+            text={country.text}
+          />
+        ))}
+      </main>
     </>
   )
 }
---
+++ Travel-Journal/src/components/Entry.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "../App.css"
+import marker from "./../assets/marker.png"
+
+function Entry(props) {
+    return (
+        <article className="journal-entry">
+            <div className="main-image-container">
+                <img src={props.img.src} alt={props.img.alt} className="main-image" />
+            </div>
+            <div className="info-container">
+                <img className="marker" src={marker} alt="map marker icon" />
+                <span className="country">{props.country}</span>
+                <a href={props.googleMapsLink}>View on Google Maps</a>
+                <h2 className="entry-title">{props.title}</h2>
+                <p className="trip-dates">{props.dates}</p>
+                <p className="entry-text">{props.text}</p>
+            </div>
+        </article>
+    )
+}
+
+export default Entry;
