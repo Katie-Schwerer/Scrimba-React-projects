112a42c875cb376d9eb1ce9acab5a0e5459ed7eb
Katie Schwerer <89220941+Katie-Schwerer@users.noreply.github.com>
2026-09-20 00:29:46 +0000

    added a functipnality to the input to add new items.

--- chef-claude/src/components/Main.jsx
+++ chef-claude/src/components/Main.jsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "../App.css";
 
 function Main() {
+    const [ingredients, setIngredients] = useState([]);
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget);
+        const newIngredient = formData.get("ingredient");
+        setIngredients(prevIngredient => [...prevIngredient, newIngredient])
+    }
+
     return (
         <main>
-            <form>
-                <input type="text" placeholder="e.g. Oregano" aria-label="Add ingredient" />
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder="e.g. Oregano" aria-label="Add ingredient" name="ingredient" />
                 <button>Add Ingredient</button>
             </form>
+            <ul>
+                {ingredients.map((ingredient, index) => (
+                    <li key={index}>{ingredient}</li>
+                ))}
+            </ul>
         </main>
     )
 }
